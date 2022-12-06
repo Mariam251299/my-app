0862a8bb89588d80151cd88fbe0abf4a17246bdb
LuisNocedal <luisnocedal1119@gmail.com>
2022-12-06 05:36:59 -0600

    tables

--- src/components/steps/Step2.js
+++ src/components/steps/Step2.js
@@ -1,17 +1,66 @@
-import { Grid } from "@mui/material";
+import { Card, CardContent, Grid } from "@mui/material";
 import React, { useState } from "react";
+import DataTable from 'react-data-table-component';
 
-const Step2 = ({databases}) => {
-
+const Step2 = ({ databases }) => {
     console.log(databases);
+
+    const getData = (tableData) => {
+        // console.log(tableData);
+
+        return tableData.map((row, index) => {
+            return {
+                id: index,
+                ...row
+            }
+        })
+    }
+
+    const getColumns = (tableData) => {
+
+        return Object.entries(tableData[0]).map((col) => {
+            // console.log(col);
+            return {
+                name: col[0],
+                selector: row => row[col[0]],
+                sortable: true,
+            }
+        });
+    }
 
     return (
         <div>
-            <Grid container spacing={2} columns={{ xs: 4, md: 12 }}>
-                <Grid item xs={6}>
-                    <p>Paso 2</p>
-                </Grid>
-            </Grid>
+            {
+                databases.map(({ database, data }, index) => {
+                    return (
+                        <div key={index}>
+                            <p className="title">{database}</p>
+                            <Grid container spacing={2} columns={{ xs: 4, md: 12 }}>
+                                {
+                                    data.map(({ data: tableData, table_name }, index) => {
+                                        // console.log(tableData)
+                                        return (
+                                            <Grid item xs={6} key={index}>
+                                                <Card style={{minHeight: 700}}>
+                                                    <CardContent>
+                                                        <p className="subTitle">{table_name}</p>
+                                                        <DataTable
+                                                            columns={getColumns(tableData)}
+                                                            data={getData(tableData)}
+                                                            pagination
+                                                        />
+                                                    </CardContent>
+                                                </Card>
+                                            </Grid>
+                                        )
+                                    })
+                                }
+                            </Grid>
+                        </div>
+
+                    )
+                })
+            }
         </div>
     );
 }
